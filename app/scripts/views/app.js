8b828e1053ea96f15f8d0d6e370ee67c65cded42
Jonathan Kemp <jonathan.kemp@cellularsales.com>
2013-02-11 15:13:00 -0500

    Refactoring.

--- app/scripts/views/app.js
+++ app/scripts/views/app.js
@@ -2,33 +2,18 @@ var app = app || {};
 
 app.AppView = Backbone.View.extend({
 
-  el: '.container',
-
-  template: _.template( $('#hello-template').html() ),
-
   initialize: function(){
-    this.render();
 
-    //var Items = new app.ItemList;
+    this.listenTo( window.app.Items, 'add', this.updateListview );
 
-    //this.listenTo(Items, 'add', this.updateListview);
-
-    //Items.add([item1, item2]);
-  },
-
-  render: function() {
-    this.$el.html( this.template );
-
-    return this;
   },
 
   updateListview: function(item) {
 
-    var itemView = new ItemView({
+    var itemView = new app.ItemView({
       model: item
     });
 
-    $('ul').append( itemView.render().el );
+    $('#items').append( itemView.render().el );
   }
-
 });
